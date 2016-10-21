2ba1375f76f33827f54886c80ac96cc5c76a33d0
Stinni <kristinnf13@ru.is>
2016-10-21 00:30:14 +0000

    finished

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+// Assignment 8, weeks 9-10
+// Student: Kristinn Heiðar Freysteinsson
+// Email: kristinnf13@ru.is
 
 const mongoose = require("mongoose");
-const api = require("./api");
+const api = require("./api/api");
 const db = "localhost/app";
 const port = 4000;
 
 mongoose.connect(db);
 mongoose.connection.once("open", () => {
-	console.log("\x1b[36mConnected to database: " + db + "\x1b[0m");
+	console.log("\x1b[36m" + "Connected to database: " + db + "\x1b[0m");
 	api.listen(port, () => {
-		console.log("\x1b[31mWeb server started on port: " + port + "\x1b[0m");
+		console.log("\x1b[31m" + "Web server started on port: " + port + "\x1b[0m");
 	});
 });
